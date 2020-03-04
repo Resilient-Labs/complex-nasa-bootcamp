0de711b6e46b143b163d04afdc0877a279b2207c
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-04 15:08:33 -0500

    yes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 // Complex NASA API
+
+
 function getLocation(){
-fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
+fetch(`https://data.nasa.gov/resource/gvk9-iz74.json`)
 .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
     .then(response => {
       let zipCode = [];
       response.forEach((el)=> {
-        // locations.push(el.location.human_address.city.toString());
-        // try to fix in order to extract the zip code which are wrapped in a string
-        zipCode.push(el.zipcode)
+        zipCode.push(el.zipcode);
         // WE ALSO NEED of zip code we can get the center name from response.center
-        console.log(zipCode)
-        console.log(typeof zipCode[342])
-        console.log(zipCode[342])
+        document.querySelector('p').innerHTML= zipCode;
       })
     })
     .catch(err => {
@@ -22,18 +20,51 @@ fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
 
   }
 
-function getWeather(){
-  // need to create a quick lil forEach that grabs the zipcode, plugs into the api, and just console.log the response for each reported zip code
-  // need a new API that takes in zip codes
-  fetch('https://weather.ls.hereapi.com/weather/1.0/report.json?product=observation&zipcode=32899&oneobservation=true&apiKey=H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs')
-  .then(res2 => res2.json()) // parse response as JSON (can be res.text() for plain response)
-  .then(response2 => {
-    console.log(response2)
-   })
-   .catch(err2 => {
-       console.log(`error ${err2}`)
-       alert("sorry, there are no results for your search")
-   });
+function getWeather(love){
+      inputZip();
+      singleZip();
+
+      fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${love},us&appid=9f46d0f653902e57a2d6d49da811c971`)
+      .then(res2 => res2.json()) // parse response as JSON (can be res.text() for plain response)
+      .then(response2 => {
+          console.log(response2.main.temp)
+        })
+       .catch(err2 => {
+           console.log(`error ${err2}`)
+           alert("sorry, there are no results for your search")
+       });
+    }
+
+function inputZip(){
+    let zipCodeInput = ["32899", "23681-2199", "32899", "35812", "35812", "32899", "32899", "91109", "91109", "32899", "91109", "39529-6000", "35812", "23337-5099", "35812", "35812", "91109", "91109", "20771", "70189", "20546"];
+      let zipCodeTEST2 =[];
+          zipCodeInput.forEach((el) => {
+            let elNew = parseFloat(el,10)
+            zipCodeTEST2.push(elNew);
+            console.log(zipCodeTEST2)
+      })
+    }
+
+function singleZip(){
+  inputZip(zipCodeTEST2);
+  zipCodeTEST2.forEach((el) =>{
+    let love = this;
+  })
+
 }
 
-getLocation();
+
+
+
+      getLocation();
+      // inputZip();
+
+// function inputZip(){
+//   let zipCodeInput = ["32899", "23681-2199", "32899", "35812", "35812", "32899", "32899", "91109", "91109", "32899", "91109", "39529-6000", "35812", "23337-5099", "35812", "35812", "91109", "91109", "20771", "70189", "20546"];
+//     let zipCodeTEST2 =[];
+//     zipCodeInput.forEach((el) =>{
+//       let elNew = parseFloat(el,10)
+//       zipCodeTEST2.push(elNew);
+//       console.log(zipCodeTEST2);
+// }
+// }
