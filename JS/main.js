92632cf98c2105fb4b43149f213b05bf03bdd5e5
Victor Tran <vicphitran@gmail.com>
2020-03-07 15:41:45 -0500

    first commit

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,3 @@
-// let userPickZipCode = prompt("Type in the desired zipcode!")
-
-// let key = "sprJoZGnAe54FVQ3yTZDa7VwxygBUNeiqKYrQO35"
-
 function newItem(site, weather) {
   const ul = document.querySelector('#addInput')
   console.log(site, weather)
@@ -17,21 +13,16 @@ function newItem(site, weather) {
 fetch(`https://data.nasa.gov/resource/gvk9-iz74.json`)
   .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
   .then(nasaSites => {
-    // console.log(nasaSites.forEach((center) => { //for each will call
-    //  console.log(center.zipcode)
     for (let i = 0; i < nasaSites.length; i++) {
-    // for (let i = 0; i < nasaSites.length; i++) {
       console.log(nasaSites[i])
       fetch(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=426a3cf867fe4b93995205328200303&q=${nasaSites[i].zipcode}&num_of_days=7&tp=3&format=json`)
         .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
         .then(weather => {
           newItem(nasaSites[i], weather) //Passing in two different apis. Getting the site location and the weather of the sites
           // newItem()
-
         })
         .catch(err => {
           console.log(`error ${err}`)
-          //alert("sorry, there are no results for your search")
         });
     }
   });
