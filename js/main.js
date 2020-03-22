8ff712e0f18bbfd80e081bbff05e2e19a97f738d
Vanessa Charles <vanessacharles@Vanessas-MacBook-Air.local>
2020-03-22 15:50:13 -0400

    complex nasa 2nd push

--- js/main.js
+++ js/main.js
@@ -1,49 +1,48 @@
-let count = 0
-function getLocation(){
-fetch("https://data.nasa.gov/resource/gvk9-iz74.json")
-    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-    .then(response => {
-      // let zipcode = []
-      // let center = []
-      response.forEach((el)=>{
-        // zipcode.push(el.zipcode)
-      // document.querySelector('p').textContent=zipcode
-      if(count < 10 ){
-        getWeather(el.zipcode, el.center, el.facility )
-        count++
-      }
-      // getWeather(el.zipcode)
-      console.log(el.zipcode)
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-        alert("sorry, there are no results for your search")
-    });
-})
-}
-function getWeather(zipcode, center, facility){
-  // let url =
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${zipcode}&APPID=e71acf2c8f36d9b1df7f78ba278857d3`)
-    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-    .then(response => {
-      if(response.main.temp){
-        console.log(response.main.temp)
-        let fahrenheit = Math.floor((response.main.temp - 273.15) * 1.8 + 32) + "F"
-        document.querySelector('.temp').innerHTML+=fahrenheit
-        document.querySelector('p').textContent+=zipcode
-        document.querySelector('.center').textContent+=center
-        document.querySelector('.facility').textContent+=facility
-        console.log(fahrenheit);
-        console.log(center);
-        console.log(facility);
-        console.log(zipcode);
-      }
-      // console.log(response.main);
-    })
-      // .catch(err => {
-      //     console.log(`error ${err}`)
-      //     alert("sorry, there are no results for your search")
-      // });
-}
+//Collaborators: Eric Espinoza, Nyah Macklin, Asiah Bennett, Vanessa Charles,and Joshua Wilkerson
+//
 
-getLocation()
+function getWeatherByZip(nasaLocation){
+    const {zipcode, center, city} = nasaLocation;
+
+   return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=e71acf2c8f36d9b1df7f78ba278857d3&units=imperial`)
+
+    .then(response => response.json())
+    .then(weatherData=> ({
+      temp: weatherData.main.temp,
+      center,
+      city,
+      zipcode
+    })
+
+    )
+  }
+    fetch(`https://data.nasa.gov/resource/gvk9-iz74.json`)
+    .then(response =>response.json())
+    .then(nasaLocations=>{
+      for(const nasaLocation of nasaLocations){
+        console.log(nasaLocations)
+         getWeatherByZip(nasaLocation)
+         .then(tempForLocation=>{
+           const { center, city, zip, temp } = tempForLocation
+
+           let sec1 = document.createElement(`section`)
+           let sec2 = document.createElement(`section`)
+           let sec3 = document.createElement(`section`)
+
+           sec1.textContent = center
+           sec2.textContent = city
+           sec3.textContent = temp
+
+           sec1.classList.add(`facility`)
+           sec2.classList.add(`facility`)
+           sec3.classList.add(`facility`)
+
+           document.querySelector(".data").appendChild(sec1)
+           document.querySelector(".data").appendChild(sec2)
+           document.querySelector(".data").appendChild(sec3)
+
+
+         })
+        // break
+      }
+   })
