539f94fa362c66923f0245c5d612b5199e877905
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-03-06 20:19:45 -0500

    final draft complex nasa/weather api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
+// This is weather tester
+//  let zipcode ="08021"
+// let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=3c92daba6ae0bc37e9d2bea7e211cba0`
+// fetch(url)
+// .then(res => res.json())
+// .then(data => {
+//   console.log(data);
+// })
+//error regarding temp of undefined?,
+
 let url = `https://data.nasa.gov/resource/gvk9-iz74.json`
-let url2 = ``
+
 fetch(url)
 .then(res => res.json())
 .then(data => {
+  console.log(data);
   for(i=0; i<data.length; i++){
+    let facilityName = data[i].center
+    let zipcode = data[i].zipcode
+    let url2 = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=3c92daba6ae0bc37e9d2bea7e211cba0`
+
     fetch(url2)
     .then(res => res.json())
-    .then(data2 => {
-      let facilityName = data[i].center
-      let facilityLocation = data[i].zipcode
+    .then(results => {
+
+      let facilityWeather = results.main.temp
 
       let row = document.createElement('tr')
       let column1 = document.createElement('td')
@@ -17,9 +32,11 @@ fetch(url)
 
       document.querySelector("tbody").appendChild(row)
       row.appendChild(column1)
-      column1.innerText = facilityName
       row.appendChild(column2)
-      column2.innertext = facilityLocation
+      row.appendChild(column3)
+      column1.innerText = facilityName
+      column2.innerText = zipcode
+      column3.innerText = facilityWeather + `℉, ` + results.weather[0].description
 
     })
     .catch(err => {
@@ -31,9 +48,3 @@ fetch(url)
 .catch(err =>{
   console.log(`error ${err}`)
 })
-
-
-
-
-//center is name
-//zipcode is location
