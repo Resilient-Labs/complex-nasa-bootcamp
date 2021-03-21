85a9ab49c2b141228605507f85796e7c08f0c175
Zuri Walters <walterszuri02@gmail.com>
2021-03-21 08:42:34 -0400

    Second Commit

--- js/main.js
+++ js/main.js
@@ -6,41 +6,44 @@ user will input state
 */
 //Mark helped//
 //House Moses!!!!//
-const url1 = `https://data.nasa.gov/resource/gvk9-iz74.json`
-fetch(url1)
-  .then (res=> res.json())
-  .then (data=>{
+
+  const url1 = `https://data.nasa.gov/resource/gvk9-iz74.json`
+  fetch(url1)
+  .then (res => res.json())
+  .then (data => {
+    console.log(data)
     // console.log(data)
     //replace 3 for data.length for testing//
-    for (i = 0 ; i < 3; i ++){
+    for (i = 0 ; i < data.length; i ++){//runs through each element of array i is each element in arrray
+      const facilityName = data[i].center//using to extract and hold variables
       const location = data[i].zipcode
-      const url2= `https://api.openweathermap.org/data/2.5/weather?zip=${location},US&appid=b4a8249fdb18e85af658ae914f292e78`
+      const url2 = `https://api.openweathermap.org/data/2.5/weather?zip=${location}&appid=b4a8249fdb18e85af658ae914f292e78`
       //nested insside first fetch;
-      fetch(url2)
-        .then (res => res.json())
-        .then (data2 =>{
-      const facility = data[i].center
-      // const location = data[i].zipcode
+    fetch(url2)
+    .then (res => res.json())//get responce and formating it into json file
+    .then (data2 =>{
+      console.log(data2)
+      let facilityWeather = data2.main.temp
+
       const row = document.createElement('tr')
       const facilityCell = document.createElement('td') //creating each cell of the row(tr)//
       const locationCell = document.createElement('td')
       const weatherCell = document.createElement('td')
-      document.querySelector('tbody').appendChild(row) // row is a variable representing the tr which is the child of tbody//
-      row.appendChild(facilityCell)
-      //row is already made from line 18, since its not made in the HTML, we can just use dot notation. Analogy:  "row, is like out subject of the sentence","appendChild is like the verb" and "faciltyCell would like like the object"//
-      facilityCell.innerHTML = facility
-       // ^connecting cell created on line 19 to the data from line 16//
-      row.appendChild(locationCell)
-      locationCell.innerHTML = location
 
+      document.querySelector('tbody').appendChild(row)
+      row.appendChild(facilityCell)
+      row.appendChild(locationCell)
       row.appendChild(weatherCell)
-      const weather = data2[i].temp
-      weatherCell.innerHtml =
-      console.log(places)
-      console.log(facility,row)
+
+      faciltyCell.innerText = facilityName
+      locationCell.innerText = location
+      weatherCell.innerText = facilityWeather + data2.weather[0].description
+    })//second Fetch
+    .catch(err =>{
+      console.log(`error ${err}`)
     })
-  }
-  })
+    }//for loop
+  })//First fetch
   .catch(err =>{
-    alert(`error ${err}`)
+    console.log(`error ${err}`)
   })
