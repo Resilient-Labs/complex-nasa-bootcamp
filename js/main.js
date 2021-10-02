4458a4e5d3d9f3ec3dacd592d866999281a57b77
Enaisha Savage <89624071+Enaisha15@users.noreply.github.com>
2021-10-02 18:33:56 -0400

    Update main.js
    // //collaborated with Team Hayden to successfully complete team projects 
    Redacted key

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
-fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
+// //collabarted with Team hayden to succesfully complete team projects 
+
+fetch('https://data.nasa.gov/resource/().json')
 .then(res => res.json()) // parse response as JSON
 .then(data => {
   console.log(data)
@@ -13,7 +15,7 @@ fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
   let lat = data[i].location.latitude
   let lon = data[i].location.longitude
 
-  const url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=822b8faf46b7ece382f979b9814116ba
+  const url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=()
   `)
   fetch(url)
       .then(res => res.json()) 
@@ -34,37 +36,4 @@ fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
 .catch(err => {
     console.log(`error ${err}`)
 });
-  // fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
-  // .then(res => res.json()) // parse response as JSON
-  // .then(data => {
-  //   console.log(data)
-  //   for(let i = 0; i < data.length; i++){
-  //   let add = document.createElement('tr')
-  //   add.innerHTML = `<td>${data[i].facility}</td><td>${data[i].city}</td><td>${data[i].state}</td>`
-  //   document.querySelector('table').appendChild(add)
-
-
-
-  //   let lat = data[i].location.latitude
-  //   let lon = data[i].location.longitude
-
-  //   const url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7d5cbe693362dd5f1d98eb3d6e7640e9`)
-  //   fetch(url)
-  //       .then(res => res.json()) // parse response as JSON
-  //       .then(data => {
-  //         console.log(data)
-  //         let tFar = Math.floor((data.main.temp - 273.15) * 9/5 + 32)
-  //         add.innerHTML += `<td>${tFar}</td>`
-  //       })
-  //       .catch(err => {
-  //           console.log(`error ${err}`)
-  //       });
-
-  //   }
-
-
-
-  // })
-  // .catch(err => {
-  //     console.log(`error ${err}`)
-  // });
+  // //collabarted with Team hayden to succesfully complete team projects 
