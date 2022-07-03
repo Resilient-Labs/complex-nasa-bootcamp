f9376bd777e1b3a7b9dd357385042d4b2bbd67ae
feesah22 <nafeesah.q.shah@gmail.com>
2022-07-02 21:14:07 -0400

    test

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function returnData() {
         .then(res => res.json())
         .then(data => {
             for (let i = 0; i < data.length; i++) {
-                console.log(data[i].center)
+                console.log(data.length)
 
                 let stationName = document.createTextNode(data[i].center)
                 let li = document.createElement('li')
@@ -14,9 +14,10 @@ function returnData() {
                 
                 document.querySelector('ul').appendChild(li)
 
-                fetch(`http://api.weatherapi.com/v1/current.json?key=bd74c761729d46fe81a03455212909&q=${data[i].city}`)
-                    .then(res => res.json())
+                fetch(`http://api.weatherapi.com/v1/current.json?key=f2a2c0355c0e436db46231502220207&q=${data[i].city}`)
+                    .then(res => res.json()) 
                     .then(data => {
+                        
                         console.log(data.current)
                         
                         let weatherTemp = document.createTextNode(`${data.current.condition.text} and ${data.current.temp_f}f`)
