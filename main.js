f4f88cf6b3d23019d423792dc51274a7370bbd52
Pedro Leonardo <pedro.leonardo95@icloud.com>
2022-10-17 12:41:07 -0400

    needs more css

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// api_key=37EGXi0BUJCqJOfREBAp3bvTFbnGvmI7IhsJeKY4
+// real one 
 
 document.querySelector("button").addEventListener("click", myWeather);
 
@@ -17,21 +17,34 @@ function myWeather() {
         const long = data[i].location.longitude;
         console.log(long);
 
-        document.querySelector("#facility").innerText = data[i].facility;
-        document.querySelector("#city").innerText = data[i].city;
-        document.querySelector("#state").innerText = data[i].state;
-        document.querySelector("#country").innerText = data[i].country;
-        let facilityName = data[i].facility
+        let facilityName = data[i].facility;
+        let cityName = data[i].city;
+        let stateName = data[i].state;
+        let countryName = data[i].country;
 
-       
-          let li = document.createElement("li");
-          li.innerText = facilityName;
-        
-          let toDoContainer = document.querySelector("#toDoContainer");
-          toDoContainer.appendChild(li);
-          
-        
-      
+        let li = document.createElement("li");
+        li.innerText = facilityName;
+
+        let liCity = document.createElement("li");
+        liCity.innerText = cityName;
+
+        let liState = document.createElement("li");
+        liState.innerText = stateName;
+
+        let liCountry = document.createElement("li");
+        liCountry.innerText = countryName;
+
+        let toDoContainer = document.querySelector("#toDoContainer");
+        toDoContainer.appendChild(li);
+
+        let toDoContainerOne = document.querySelector("#toDoContainerOne");
+        toDoContainerOne.appendChild(liCity);
+
+        let toDoContainerTwo = document.querySelector("#toDoContainerTwo");
+        toDoContainerTwo.appendChild(liState);
+
+        let toDoContainerThree = document.querySelector("#toDoContainerThree");
+        toDoContainerThree.appendChild(liCountry);
 
         fetch(
           `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lati}&lon=${long}`
@@ -45,14 +58,23 @@ function myWeather() {
                 .air_temperature;
 
             const tempF = temp * 1.8 + 32;
+            const tempFNew = Math.round(tempF);
 
-            console.log(Math.round(tempF));
-            document.querySelector("#temp").innerText = tempF;
+            // console.log(Math.floor(tempF));
+
+            let temptName = tempFNew + "°F";
+
+            let liTempt = document.createElement("li");
+            liTempt.innerText = temptName;
+
+            let toDoContainerFour =
+              document.querySelector("#toDoContainerFour");
+            toDoContainerFour.appendChild(liTempt);
+
           });
       }
     });
 }
-
 
 // from my todo list
 // let toDoContainer = document.querySelector("#hi");
