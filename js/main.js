225d5b18a56c07661196c16e7231559e68a45bf7
Marsha Tiisa <100469351+marshatiisa@users.noreply.github.com>
2022-10-16 20:06:18 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Goal: Use NASA's API to return all of their facility locations (~400). Display the name of the facility, its location, and the weather at the facility currently.
-// got help from
+// got help from Elvin and Isaiah
 document.querySelector('button').addEventListener('click',getInfo)
 
 function getInfo() {
