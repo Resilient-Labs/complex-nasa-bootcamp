d761e7d0f4ba230f7d7ee4383340f34369d008b7
Valery Gonzalez <valgonzr@gmail.com>
2023-06-08 15:26:21 -0400

    picture fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 document.querySelector('button').addEventListener('click', getLocation)
 const btn = document.querySelector('button')
 const ul = document.querySelector('ul')
+// require('dotenv').config()
+
+// require('dotenv').config({ path: require('find-config')('.env') })
+
+// console.log(process.env)
+
+// require('dotenv').config()
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+
+// console.log(process.env)
 
 function getLocation() {
   const url = (`https://data.nasa.gov/resource/gvk9-iz74.json`)
@@ -16,9 +27,9 @@ function getLocation() {
 
         let lat = data[i].location.latitude
         let lon = data[i].location.longitude
-
-        // const temp = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c943ad3b8c08641ee54e526e620be0ca`
-        const temp =`http://api.weatherapi.com/v1/current.json?q=${lon},${lat}&key=488a0f4271c04fb9a1221953230604`
+        const key = '488a0f4271c04fb9a1221953230604'
+      
+        const temp =`http://api.weatherapi.com/v1/current.json?q=${lon},${lat}&key=${key}`
 
         fetch(temp)
           .then(res => res.json()) // parse response as JSON
