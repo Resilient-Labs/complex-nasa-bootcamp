603fa9763c7b2eb45189bc810d1f7f7ebfb8b7cd
socratic617 <erik.solis02135@gmail.com>
2023-10-22 03:06:03 -0400

    Added bootstrap and table

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,45 +6,82 @@ document.querySelector("button").addEventListener("click", getFacilityNameWeathe
 
 
 function getFacilityNameWeather() {
+  document.querySelector(".table").id = "hidden";
 
-  const urlNasa = `https://data.nasa.gov/resource/gvk9-iz74.json`;
+  const urlNasa = 'https://data.nasa.gov/resource/gvk9-iz74.json';
   
 
   //using method fetch to request my data from that api using url
   fetch(urlNasa) // go get api
-    
-    .then((res) => res.json())
+    .then((res) =>  res.json())
     .then((data) => {
     
-      console.log(data)
-            
-      data.forEach((facility)=>{ 
-        // console.log(
-        //   `Facility Name: ${facility.center}  Zipcode: ${facility.zipcode}`
-        // );
-  let facilityCenter = facility.center;
-  let zipCode = facility.zipcode;
-  const urlZip = `http://api.weatherapi.com/v1/current.json?key=eb88852a78b84a5ab07191927231810&q=${zipCode}`;
-  fetch(urlZip) // go get api
-
-  //.then property passes param res which responds with JSON parsing out the data you requested 
-    .then((res) => res.json())
-    .then((data) => {
-
-      // console.log("Center Weather: " + JSON.stringify(data)); 
-      let listFacility = document.createElement('li');
-      let temp = Math.floor((data.current.temp_f))
-      let fTemp = `${temp}°F`;
+      console.log(data);
       
-      let text = document.createTextNode(`${facilityCenter} ${fTemp}`)
-      listFacility.appendChild(text)
-      document.querySelector('#list').appendChild(listFacility)
+      let index = 1; // Counter for Table Rows
+      
+      data.forEach((facility)=>{ 
+        console.log(facility);
+        let facilityCenter = facility.center;//calling api for facilities
+        let zipCode = facility.zipcode;//callling api for zipcode
 
+        const urlZip = `http://api.weatherapi.com/v1/current.json?key=eb88852a78b84a5ab07191927231810&q=${zipCode}`;
+
+        fetch(urlZip) // go get api
+          .then((res) => res.json()) //.then property passes param res which responds with JSON parsing out the data you requested
+          .then((data) => {
+            let temp = Math.floor(data.current.temp_f); //using method to round current temp of F
+            let fTemp = `${temp}°F`;
+
+            // let listFacility = document.createElement("li");
+            // let text = document.createTextNode(`${facilityCenter} ${fTemp}`);
+            // listFacility.appendChild(text);
+            // document.querySelector("#list").appendChild(listFacility);
+
+            // <tr>
+            //   <th scope="row">1</th>
+            //   <td>Mark</td>
+            //   <td>Otto</td>
+            // </tr>
+
+            let tableIndex = document.createElement("th"); // creating ELement table header for table
+            tableIndex.scope = "row"; //
+            tableIndex.innerText = index; //start at index 1
+            index++; // then increment as we add
+
+            // console.log("th element:" );
+            // console.log(tableIndex);
+
+            let tableDataOne = document.createElement("td");
+            tableDataOne.innerText = facilityCenter;
+            // console.log("facility Name :")
+            // console.log(tableDataOne);
+
+            let tableDataTwo = document.createElement("td");
+            tableDataTwo.innerText = fTemp;
+
+            let tableDataThree = document.createElement("td");
+            tableDataThree.innerText = zipCode;
+
+            let tableRow = document.createElement("tr");
+            tableRow.appendChild(tableIndex);
+            tableRow.appendChild(tableDataOne);
+            tableRow.appendChild(tableDataTwo);
+            tableRow.appendChild(tableDataThree);
+
+            console.log("Table Row :")
+            console.log(tableRow)
+
+            document.querySelector("tbody").appendChild(tableRow);
+
+
+          })
+          .catch((err) => {
+            console.log(`error ${err}`);
+          });
+      })
+
+      document.querySelector("#hidden").id = "";
     })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
-  })
-})
 }
 
