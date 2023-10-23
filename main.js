df39f729256fee4b4f83f2d0d0c9aafd443cb9ec
matthewfermo <matthew.t.fermo@gmail.com>
2023-10-23 12:59:23 -0400

    scrapped last one to upload this one

--- main.js
+++ main.js
@@ -1,89 +1,97 @@
-let apiKey = "04d38a269b124b12b7b810cc1df607bb";
+// Goal: Use NASA's API to return all of their facility locations (~400). Display the name of the facility, its location, and the weather at the facility currently.
+document
+  .querySelector("#facilitiesBtn")
+  .addEventListener("click", getNasaFacilities);
 
-fetch(`https://data.nasa.gov/resource/gvk9-iz74.json`)
-  .then((res) => res.json()) // parse response as JSON
-  .then((data) => {
-    //This loop removes the NASA facilities that share the same center, since those facilities would have the same temperature
-    let arr = [];
-    let arr2 = [];
-    for (let i = 0; i < data.length - 1; i++) {
-      if (!arr.includes(data[i].center)) {
-        arr.push(data[i].center);
-        arr2.push(data[i]);
+function getNasaFacilities() {
+  const facilityUrl = "https://data.nasa.gov/resource/gvk9-iz74.json";
+  const weatherAPIKey = "9ee140de1f9f43735062a4c0d89dfe0c"; // api key
+  document.querySelector("#results").innerHTML = "";
+  const table = document
+    .querySelector("#results")
+    .appendChild(document.createElement("table"));
+  table.classList.add("nasaFacilitiesTable");
+
+  // Create table headings
+  let headingsRow = table.appendChild(document.createElement("tr"));
+
+  let headingName = headingsRow.appendChild(document.createElement("th"));
+  headingName.innerText = "Facility Name";
+
+  let headingType = headingsRow.appendChild(document.createElement("th"));
+  headingType.innerText = "Facility Type";
+
+  let headingCity = headingsRow.appendChild(document.createElement("th"));
+  headingCity.innerText = "City";
+
+  let headingState = headingsRow.appendChild(document.createElement("th"));
+  headingState.innerText = "State";
+
+  let headingZip = headingsRow.appendChild(document.createElement("th"));
+  headingZip.innerText = "ZIP Code";
+
+  let headingLat = headingsRow.appendChild(document.createElement("th"));
+  headingLat.innerText = "Latitude";
+
+  let headingLong = headingsRow.appendChild(document.createElement("th"));
+  headingLong.innerText = "Longitude";
+
+  let headingWeather = headingsRow.appendChild(document.createElement("th"));
+  headingWeather.innerText = "Weather";
+
+  fetch(facilityUrl)
+    .then((facilityRes) => facilityRes.json()) // parse response as JSON
+    .then((facilityData) => {
+      for (let i = 0; i <= facilityData.length; i++) {
+        const facilityName = facilityData[i].center;
+
+        const facilityType = facilityData[i].facility;
+        const facilityCity = facilityData[i].city;
+        const facilityState = facilityData[i].state;
+        const facilityZip = facilityData[i].zipcode;
+        const facilityLat = facilityData[i].location.latitude;
+        const facilityLong = facilityData[i].location.longitude;
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${facilityLat}&lon=${facilityLong}&appid=${weatherAPIKey}`;
+
+        // look for distinct values for facilityName
+        // add data to an array and remove the dupes
+
+        fetch(weatherUrl)
+          .then((weatherRes) => weatherRes.json())
+          .then((weatherData) => {
+            const facilityWeather = Math.round(
+              (weatherData.main.temp - 273.15) * (9 / 5) + 32
+            );
+
+            // create table row
+            let row = table.appendChild(document.createElement("tr"));
+
+            // Add table cells for facility information
+            let cellName = row.appendChild(document.createElement("td"));
+            cellName.innerText = facilityName;
+
+            let cellType = row.appendChild(document.createElement("td"));
+            cellType.innerText = facilityType;
+
+            let cellCity = row.appendChild(document.createElement("td"));
+            cellCity.innerText = facilityCity;
+
+            let cellState = row.appendChild(document.createElement("td"));
+            cellState.innerText = facilityState;
+
+            let cellZip = row.appendChild(document.createElement("td"));
+            cellZip.innerText = facilityZip;
+
+            let cellLat = row.appendChild(document.createElement("td"));
+            cellLat.innerText = facilityLat;
+
+            let cellLong = row.appendChild(document.createElement("td"));
+            cellLong.innerText = facilityLong;
+
+            let cellWeather = row.appendChild(document.createElement("td"));
+            cellWeather.innerText = `${facilityWeather}°F`;
+          });
       }
-    }
-    //Centers are sorted alphabetically
-    arr2.sort((a, b) => a.center.localeCompare(b.center));
-
-    /*
-    //If you want to see all 485 facilities, uncomment the following code block & line 38. Comment out line 37 & lines 8-17
-        let arr2 =[]
-        for (let i = 0; i < data.length - 1; i++){ 
-            arr2.push(data[i])
-        }
-
-        //Alphabetize the centers by name
-        arr2.sort((a,b) => a.facility.localeCompare(b.facility))
-*/
-
-    //Create and append the list items to the DOM
-    let list = document.querySelector("ul");
-    arr2.forEach((item) => {
-      let listItems = document.createElement("li");
-      listItems.addEventListener("click", getZipcode);
-      listItems.innerText = ` ${item.center}, ${item.city}, ${item.state}`;
-      //listItems.innerText = ` ${item.facility}, ${item.center}, ${item.city}, ${item.state}`
-      list.appendChild(listItems);
-
-      //removing zips longer than 5 digits
-      if (item.zipcode.length > 5) {
-        listItems.value = item.zipcode.slice(0, 5);
-      } else {
-        listItems.value = item.zipcode;
-      }
-    });
-
-    function getZipcode(e) {
-      let zip = e.target.value;
-      getTemp(zip);
-    }
-  })
-
-  .catch((err) => {
-    console.log(`error ${err}`);
-  });
-
-//Weather API
-
-function getTemp(zip) {
-  fetch(
-    `https://api.weatherbit.io/v2.0/current?key=${apiKey}&units=i&postal_code=${zip}&country=US`
-  )
-    .then((res) => res.json()) // parse response as JSON
-    .then((info) => {
-      console.log(info);
-      document.querySelector(
-        "#city"
-      ).innerText = `${info.data[0]["city_name"]}, ${info.data[0]["state_code"]}`;
-      document.querySelector(
-        "#description"
-      ).innerText = `${info.data[0].weather.description}`;
-      document.querySelector(
-        "#farenheit"
-      ).innerText = `${info.data[0].temp} °F`;
-      //   document.querySelector("#precip").innerText = `${info.data[0].precip} %`;
-      //   document.querySelector("#uv").innerText = `${Math.round(
-      //     info.data[0].uv
-      //   )}`;
-      //   document.querySelector(
-      //     "#humidity"
-      //   ).innerText = `Humidity: ${info.data[0].rh} %`;
-      //   document.querySelector(
-      //     "#pressure"
-      //   ).innerText = `Pressure: ${info.data[0].pres}°`;
-      //   document.querySelector(
-      //     "#windSpeed"
-      //   ).innerText = `Wind: ${info.data[0]["wind_spd"]} mph`;
     })
 
     .catch((err) => {
