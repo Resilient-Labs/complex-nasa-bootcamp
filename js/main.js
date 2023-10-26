81ad5af20881dc0ec76bf4f5dd12a929f4049064
alejandralondev <aledonolon@outlook.com>
2023-10-25 23:15:20 -0400

    fixed temp bug

--- js/main.js
+++ js/main.js
@@ -22,14 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 const longitude = facility.location.longitude 
 
                 // Fetch weather data based on latitude and longitude
-                fetch(`https://api.weatherapi.com/v1/current.json?key=b19bd926788146399f7232525232210=${latitude},${longitude}`)
+                fetch(`https://api.weatherapi.com/v1/current.json?key=0834971ddb924ff6a0323327232610&q=${latitude},${longitude}`) 
                     .then(response => response.json())
                     .then(weatherData => {
-                        // Check if required properties exist in the response
-                        if (weatherData.current && weatherData.current.temperature && weatherData.current.weather_descriptions) {
-                            const temperature = weatherData.current.temperature
-                            const description = weatherData.current.weather_descriptions[0]
-                            weatherCell.textContent = `${description}, ${temperature}°C` 
+                        console.log(weatherData)
+                        if (weatherData.current && weatherData.current.temp_c && weatherData.current.condition && weatherData.current.condition.text) { //checking to see if these objects and descriptions exist
+                            const temperature = weatherData.current.temp_c;
+                            const description = weatherData.current.condition.text;
+                            weatherCell.textContent = `${description}, ${temperature}°C`; 
                         } else {
                             weatherCell.textContent = 'Data not available'
                         }
@@ -51,3 +51,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }) 
 }) 
 
+// weatherData.current: This checks if the current property exists on the weatherData object.
+
+// weatherData.current.temp_c: If current exists, this checks if temp_c (temperature in Celsius) exists on the current object.
+
+// weatherData.current.condition: If temp_c exists, this checks if the condition object exists on the current object.
+
+// weatherData.current.condition.text: If condition exists, this checks if the text property (which holds the weather description) exists on the condition obj
